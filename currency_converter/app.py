670bbdbfee604f4d47015c7d3624f2300aeb9ed9
Mason hamilton <Hamim949@hsd.k12.or.us>
2022-06-09 15:24:27 -0700

    fixed

--- currency_converter/app.py
+++ currency_converter/app.py
@@ -3,12 +3,12 @@ import dearpygui.dearpygui as dpg
 dpg.create_context()
 dpg.create_viewport(title='Currency converter', width=600, height=300)
 
-with dpg.window(label="Currency converter"):
-    dpg.add_text("Welcome to the currency converter")
-    dpg.add_input_float(label="amount", default_value=1.0, width=10)
-    dpg.add_combo(["USD - US dollar", "EUR - Euros"], Label="From", width=200,
+with dpg.window(label="Currency converter", width=600, height=300):
+    dpg.add_text("Welcome to the currency converter.")
+    dpg.add_input_float(label="amount", default_value=1.0, width=100)
+    dpg.add_combo(["USD - US dollar", "EUR - Euros"], label="From", width=200,
         default_value="USD - US Dollar")
-    dpg.add_combo(["USD - US dollar", "EUR - Euros"], Label="To", width=200,
+    dpg.add_combo(["USD - US dollar", "EUR - Euros"], label="To", width=200,
         default_value="EUR - Euros")
     dpg.add_button(label="Convert")
     
