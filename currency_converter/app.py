87dd2c22cac17307111a5934502cdea2a489a6b3
Mason hamilton <Hamim949@hsd.k12.or.us>
2022-06-15 15:22:38 -0700

    finished api for now

--- currency_converter/app.py
+++ currency_converter/app.py
@@ -1,17 +1,41 @@
 import dearpygui.dearpygui as dpg
 import model
 
+def convert():
+    """get exchange rate & multiply by amount"""
+    amount = dpg.get_value(amount_id)
+    from_cur = dpg.get_value(from_cur_id)
+    to_cur = dpg.get_value(to_cur_id)
+
+    # get only the codes
+    from_cur = from_cur.split("-")[0].strip()
+    to_cur = to_cur.split("-")[0].strip()
+
+    status, rate = model.get_exchange_rate(from_cur, to_cur)
+    exchange = amount * rate
+    results = "{} {} is equal to {} {}".format(amount, from_cur, exchange, to_cur)
+    dpg.set_value(output_id, results)
+
 dpg.create_context()
+
+# widget tag ids
+amount_id = dpg.generate_uuid()
+from_cur_id = dpg.generate_uuid()
+to_cur_id = dpg.generate_uuid()
+output_id = dpg.generate_uuid()
+
 dpg.create_viewport(title='Currency converter', width=600, height=300)
 
 with dpg.window(label="Currency converter", width=600, height=300):
     dpg.add_text("Welcome to the currency converter.")
-    dpg.add_input_float(label="amount", default_value=1.0, width=100)
+    dpg.add_input_float(label="amount", default_value=1.0, width=100,
+        tag=amount_id, min_value=0.01, min_clamped=True)
+    dpg.add_combo(model.currencies, default_value=model.default_from, 
+        tag=from_cur_id, label="from", width=250)
     dpg.add_combo(model.currencies,
-        default_value=model.default_from, label="from", width=250)
-    dpg.add_combo(model.currencies,
-        default_value=model.default_from, label="to", width=250)
-    dpg.add_button(label="Convert")
+        tag=to_cur_id, default_value=model.default_from, label="to", width=250)
+    dpg.add_button(label="Convert", callback=convert)
+    dpg.add_text("", tag=output_id)
     
 
 dpg.setup_dearpygui()
--- currency_converter/model.py
+++ currency_converter/model.py
@@ -1,3 +1,5 @@
+import requests
+
 currency_code_map = {
     "cad": "canadian curency",
     "eur": "Euro",
@@ -20,6 +22,29 @@ def get_currency_from_code(code):
 def get_currency_string(code):
     return code + " - " + get_currency_from_code(code)
 
+url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json"
+response = requests.get(url_host)
+sample_json = response.json()
+
+def get_exchange_rate(from_code, to_code):
+    url_host = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+    endpoint = from_code + "/" + to_code
+    ext = ".json"
+    url = url_host + endpoint + ext
+    response = requests.get(url)
+    if response.ok:
+        exchange = response.json()
+        rate = exchange[to_code]
+    else:
+        rate = 0
+    return response.ok, rate
+
+
+
+def get_currency_string(code):
+    return code + " - " + get_currency_from_code(code)
+
+
 currencies = get_currencies()
 default_from = get_currency_string("usd")
 default_to = get_currency_string("eur")
@@ -27,4 +52,7 @@ default_to = get_currency_string("eur")
 if __name__ == "__main__":
     get_currencies()
     print(get_currency_string())
+
+    success, rate = get_exchange_rate("usd", "eur")
+    print(rate)
 
